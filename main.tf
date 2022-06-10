3257df42a7733e218ff9eae6e3c9e6bc1854527e
Chris Freeman <chris.freeman@digital.homeoffice.gov.uk>
2022-06-10 15:28:24 +0100

    reorder

--- main.tf
+++ main.tf
@@ -3,35 +3,11 @@ locals {
   lambda_archive_filename = "${path.module}/functions/notify_slack.zip"
 }
 
-# Bucket notification
-
-resource "aws_lambda_permission" "allow_bucket" {
-  statement_id  = "AllowExecutionFromS3Bucket"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.notify_slack.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = data.aws_s3_bucket.guardduty.arn
-}
-
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = var.bucket_name
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.notify_slack.arn
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-  }
-
-  depends_on = [aws_lambda_permission.allow_bucket]
-}
-
-#  the path the python or node.js file is stored in the directory
 data "archive_file" "notify_slack" {
   type        = "zip"
   source_file = local.lambda_filename
   output_path = local.lambda_archive_filename
 }
-
 
 resource "aws_cloudwatch_log_group" "lambda_function" {
   name              = "/aws/lambda/${var.lambda_function_name}"
@@ -69,6 +45,28 @@ resource "aws_lambda_function" "notify_slack" {
     data.archive_file.notify_slack,
     aws_cloudwatch_log_group.lambda_function
   ]
+}
+
+# Bucket notification
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notify_slack.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = data.aws_s3_bucket.guardduty.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = var.bucket_name
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.notify_slack.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "AWSLogs/"
+  }
+
+  depends_on = [aws_lambda_permission.allow_bucket]
 }
 
 
