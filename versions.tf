4dab6c242583489a4744cfa760eead66ba5437cf
samuel.walters <samuel.walters@digital.homeoffice.gov.uk>
2025-10-29 16:08:21 +0000

    try later provider version

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.70"
+      version = "~> 6.0"
     }
     archive = {
       source  = "hashicorp/archive"
